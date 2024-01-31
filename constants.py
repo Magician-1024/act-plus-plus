7518f4e3fb3c8587ea74f4c1efd5183250f92cf5
Magician-1024 <409998719@qq.com>
2024-01-31 17:53:26 +0800

    change constants clone robomimic from r2d2 branch

--- constants.py
+++ constants.py
@@ -2,11 +2,12 @@ import pathlib
 import os
 
 ### Task parameters
-DATA_DIR = '/home/zfu/interbotix_ws/src/act/data' if os.getlogin() == 'zfu' else '/scr/tonyzhao/datasets'
+#DATA_DIR = '/home/zfu/interbotix_ws/src/act/data' if os.getlogin() == 'zfu' else '/scr/tonyzhao/datasets'
+DATA_DIR = '/root/act-data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
-        'num_episodes': 50,
+        'num_episodes': 5,
         'episode_len': 400,
         'camera_names': ['top', 'left_wrist', 'right_wrist']
     },
