6fd10150759484a5bb85b1086408dc303496f7ca
Magician-1024 <409998719@qq.com>
2024-02-18 16:01:29 +0800

    error fix

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ DATA_DIR = '/home/zhr/projects/aloha/data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
-        'num_episodes': 5,
+        'num_episodes': 50,
         'episode_len': 400,
         'camera_names': ['top', 'left_wrist', 'right_wrist']
     },
